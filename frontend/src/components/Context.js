4e5a71d2298e8cfeab4ebe0f450d8e99326344a1
Eason Wei <easonwei1998@gmail.com>
2022-04-30 02:30:03 +0800

    Check first time login status

--- frontend/src/components/Context.js
+++ frontend/src/components/Context.js
@@ -2,7 +2,7 @@ import { createGlobalState } from 'react-hooks-global-state';
 
 // this is to handle the login authentication, set status to local storage to keep it stay after refreshing
 const { setGlobalState, useGlobalState } = createGlobalState({
-    user: localStorage.getItem('user')
+    user: localStorage.getItem('user') === null ? 0 : localStorage.getItem('user') // if user is not set yet, set it to 0, meaning not logged in
 });
 
 export const setUser = (s) => {
